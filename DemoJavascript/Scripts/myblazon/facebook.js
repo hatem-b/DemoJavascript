1b1188bde148c2b7f99f03010d65cf60abb71b08
hatem-b <hatem@serial-labs.com>
2014-04-10 16:25:42 +0200

    Signed-off-by: hatem-b <hatem@serial-labs.com>

--- DemoJavascript/Scripts/myblazon/facebook.js
+++ DemoJavascript/Scripts/myblazon/facebook.js
@@ -3,8 +3,31 @@
 
     var facebook =
     {
-        
-        OpenPopup: function (href, $popup) {
+        InitAsync: function (app_Id) {
+            FB.init({
+                appId: app_Id,
+                status: true,
+                cookie: true,
+                xfbml: true,
+                oauth: true,
+                fileUpload: true
+            });
+            FB.Canvas.setSize({ height: 600 });
+            window.setTimeout("FB.Canvas.setAutoGrow(100)", 500);
+            FB.Canvas.setSize({ height: 600 });
+            FB.Canvas.scrollTo(0, 0);
+            var permsNeeded = ['user_birthday', 'user_hometown', 'user_location'];
+        },
+
+        LoadAsync: (function (d) {
+            var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement('script'); js.id = id; js.async = true;
+            js.src = "//connect.facebook.net/en_US/all.js";
+            ref.parentNode.insertBefore(js, ref);
+        }(document)),
+
+        OpenPopup: function (href, $popup, hasUserLikedApp, needForceUser2LikeApp) {
             if (hasUserLikedApp == 'true') {
                 location.href = href;
             }
@@ -19,7 +42,79 @@
             else {
                 location.href = href;
             }
-        }
+        },
+
+        setFBProfilePictureVerifyPermissions : function (imgUrl) {
+        var permsNeeded = ['publish_stream'];//'user_photos'
+        FB.api('/me/permissions', function (response) {
+            var permsArray = response.data[0];
+
+            var permsToPrompt = [];
+            for (var i in permsNeeded) {
+                if (permsArray[permsNeeded[i]] == null) {
+                    permsToPrompt.push(permsNeeded[i]);
+                }
+            }
+
+            if (permsToPrompt.length > 0) {
+                promptForPerms(permsToPrompt, imgUrl, '');
+
+            } else {
+                setFBProfilePicture(imgUrl);
+            }
+        });
+
+    },
+
+        setFBProfilePicture : function (imgUrl) {
+
+        imgUrl = '@Helpers.FBMyBlazonSiteURL' + imgUrl;
+        FB.api('/me/photos', 'post', { message: 'This is my awesome shield created with the MyBlazon Facebook application!', url: imgUrl }, function (response) {
+            if (!response || response.error) {
+                alert('Error occurred: ' + response.error.message);
+            } else {
+                var uploaded_photo_id = response.id;
+                FB.api('/' + uploaded_photo_id, function (response) {
+                    if (!response || response.error) {
+                        alert('Error occurred: ' + response.error.message);
+
+                    } else {
+                        var link = response.link + '&makeprofile=1&makeuserprofile=1';
+                        window.open(link, '');
+                    }
+                });
+            }
+        });
+    },
+
+    // Re-prompt user for missing permissions
+        promptForPerms : function (perms, imgUrl, quality) {
+        FB.login(function (response) {
+            if (response.authResponse) {
+
+                FB.api('/me/permissions', function (response) {
+                    var permsArray = response.data[0];
+                    for (var i in perms) {
+                        if (permsArray[perms[i]] == null) {
+                            return false;
+                        }
+                    }
+                    if (imgUrl != '')
+                        setFBProfilePicture(imgUrl);
+                    else {
+                        //save the user email address
+                        FB.api('/me', function (me) {
+                            updateUserEmail(me.email, quality);
+
+                        });
+                    }
+                    return true;
+                });
+            } else {
+                return false;
+            }
+        }, { scope: perms.join(',') });
+    }
 
 
 
--- DemoJavascript/Scripts/myblazon/zazzle.js
+++ DemoJavascript/Scripts/myblazon/zazzle.js
@@ -3,19 +3,70 @@
 
     var zazzle =
     {
-        RecalculateZazzleParamsJS: function () {
+        RecalculateZazzleParamsJS: function (showMyName, showMyValues, firstName, lastName, w1, w2, w3, allqav, allqavcs, allqavsep, qav2, rqav) {
+
+            if (!showMyName) {
+                firstName = " ";
+                lastName = " ";
+            }
+
+            if (!showMyValues) {
+                zazzle.RecalculateZazzleURL(" ", " ", " ", " ", " ", " ", " ", " ", firstName, lastName);
+            } else {
+                zazzle.RecalculateZazzleURL(w1, w2, w3, allqav, allqavcs, allqavsep, qav2, rqav, firstName, lastName);
+            }
+
 
         },
 
-        RecalculateZazzleURL: function (w1, w2, w3, allqav, allqavcs, allqavsep, qav2, rqav, userFName, userLname) {
+        RecalculateZazzleURL: function (w1, w2, w3, allqav, allqavcs, allqavsep, qav2, rqav, firstName, lastName, zazzleURLJS, zazzleURLJSv) {
+            var urlS = zazzleURLJS.replace('$allqavcs', allqavcs).replace('$allqavsep', allqavsep);
+            urlS = urlS.replace('$w1', w1).replace('$w2', w2).replace('$w3', w3).replace('$allqav', allqav);
+            urlS = urlS.replace('$2qav', qav2).replace('$rqav', rqav);
+            urlS = urlS.replace('$yn', firstName + ' ' + lastName).replace('$fn', firstName).replace('$ln', lastName);
 
+
+            var urlSv = zazzleURLJSv.replace('$allqavcs', allqavcs).replace('$allqavsep', allqavsep);
+            urlSv = urlSv.replace('$w1', w1).replace('$w2', w2).replace('$w3', w3).replace('$allqav', allqav);
+            urlSv = urlSv.replace('$2qav', qav2).replace('$rqav', rqav);
+            urlSv = urlSv.replace('$yn', firstName + ' ' + lastName).replace('$fn', firstName).replace('$ln', lastName);
+
+            urlS = urlS.replace(/&amp;/g, '&');
+            urlSv = urlSv.replace(/&amp;/g, '&');
+
+            $('#hrefS1').attr('href', urlS);
+            $('#hrefS2').attr('href', urlS);
+            $('#hrefO1').attr('href', urlSv);
+            $('#hrefO2').attr('href', urlSv);
         },
 
-        RecalculateZazzleParamsJSNameChanged: function () {
 
-        },
 
-        ShowHideZazzleParams: function (type, element) {
+        ShowHideZazzleParams: function (type, showMyName, showMyValues, newName, urlAction) {
+            var dataString = 'showMyName=' + (showMyName ? 'true' : 'false') + '&showMyVals=' + (showMyValues ? 'true' : 'false') + '&newName=' + newName;
+
+            $.ajax({
+                type: "GET",
+                async: true,
+                url: urlAction,
+                data: dataString,
+                success: function (response) {
+                    var resp = JSON.parse(response); //console.log($(element).attr('onclick'));
+                    if (type == 'S') {
+
+                        location.href = resp[0];
+                    }
+                    if (type == 'O') {
+
+                        location.href = resp[1];
+                    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert("error");
+
+                }
+
+            });
         }
 
     };
